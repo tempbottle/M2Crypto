1d7cd08061cd9fa9826c35591acadd0eeb634528
jupeng.xia <tempbottle@gmail.com>
2014-06-09 12:41:37 +0800

    more stable.

--- build_openssl_on_mac.sh
+++ build_openssl_on_mac.sh
@@ -2,6 +2,7 @@
 
 OPENSSL_VERSION="1.0.1h"
 
+rm -rf openssl*
 curl -O http://www.openssl.org/source/openssl-$OPENSSL_VERSION.tar.gz
 tar -xvzf openssl-$OPENSSL_VERSION.tar.gz
 mv openssl-$OPENSSL_VERSION openssl_i386
@@ -15,6 +16,6 @@ cd openssl_x86_64
 ./Configure darwin64-x86_64-cc -shared
 make
 cd ../
-lipo -create openssl_i386/libcrypto.1.0.0.dylib openssl_x86_64/libcrypto.1.0.0.dylib -output libcrypto.1.0.0.dylib
-lipo -create openssl_i386/libssl.1.0.0.dylib openssl_x86_64/libssl.1.0.0.dylib -output libssl.1.0.0.dylib
+lipo -create openssl_i386/libcrypto.1.0.1.dylib openssl_x86_64/libcrypto.1.0.1.dylib -output libcrypto.1.0.1.dylib
+lipo -create openssl_i386/libssl.1.0.1.dylib openssl_x86_64/libssl.1.0.1.dylib -output libssl.1.0.1.dylib
 rm openssl-$OPENSSL_VERSION.tar.gz
